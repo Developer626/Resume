ccf8afa26f8e9403f5d759d54bcd19f7d0fc08a7
Aaron <aaron@DoNotRespond@IDontCare.net>
2019-09-05 19:43:34 -0700

    Modified name of main folder, removed unused file.

--- Python projects/Random/Prime_number_search_Sieve_of_Eratosthenes.py
+++
@@ -1,8 +0,0 @@
-# TODO Look up Sieve of Eratosthenes
-# TODO Needs a for loop to check up to the number that is currently being checked, and a second for loop to up the search.
-# TODO One my way, and one the above version way
-# TODO Documentation
-
-
-def prime_number_search_Sieve_of_Eratosthenes():
-    pass
